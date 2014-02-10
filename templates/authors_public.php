e47982bdc69019d1b6753da40aba2709d28e4742
mariyanangelov <mariyanangelov@gmail.com>
2014-02-10 15:08:05 -0800

    Create authors_public.php

---
+++ templates/authors_public.php
@@ -0,0 +1,20 @@
+ <a href="index.php">Списък</a>
+ <a href="index.php?page=add-book">Нова книга</a>
+<form method="post" action="">
+    Име: <input type="text" name="author_name" />
+    <input type="submit" value="Добави" />    
+</form>
+
+<table border='1'>
+    <tr><th>Автор</th></tr>
+
+    <?php
+    foreach ($authors as $row) {
+        echo '<tr><td>' . $row['author_name'] . '</td></tr>';
+    }
+    ?>
+
+
+</table>
+
+
