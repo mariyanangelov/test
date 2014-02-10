48517c6a371a80bcde413dceb55207d83bd5584e
mariyanangelov <mariyanangelov@gmail.com>
2014-02-10 14:43:31 -0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+defined('INC_PATH') or define('INC_PATH', realpath(dirname(__FILE__) . '/inc'));
+defined('TEMPLATES_PATH') or define('TEMPLATES_PATH', realpath(dirname(__FILE__) . '/templates'));
